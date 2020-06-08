dce5994471efb3620826bf5b6e30bef6162edfdd
vinicius-henrique-araujo <201820325@aluno.fjn.edu.br>
2020-06-08 11:05:41 -0300

    create,update,delete,findById

--- src/main/java/br/com/caririflix/web/DAO/AuthorDAO.java
+++ src/main/java/br/com/caririflix/web/DAO/AuthorDAO.java
@@ -1,28 +1,73 @@
-package  br.com.caririflix.web.DAO;
+package br.com.caririflix.web.DAO;
+
 import javax.persistence.EntityManager;
 
 import br.com.caririflix.web.DAO.util.*;
 import br.com.caririflix.web.model.Author;
 
-
-
-
 public class AuthorDAO {
-    EntityManager em = ConnectionFactory.getEntityManager();
 
     public void save(Author author) {
+        EntityManager em = ConnectionFactory.getEntityManager();
+
         try {
             em.getTransaction().begin();
             em.persist(author);
             em.getTransaction().commit();
-        
-        }
-        catch (Exception e){
+
+        } catch (Exception e) {
             em.getTransaction().rollback();
-        }finally{
-            if(em.isOpen() && em.getTransaction().isActive())
-            em.close();
+        } finally {
+            if (em.isOpen() && em.getTransaction().isActive())
+                em.close();
         }
-        
+
+    }
+
+    public void update(Author author) {
+        EntityManager em = ConnectionFactory.getEntityManager();
+
+        try {
+            em.getTransaction().begin();
+            em.persist(author);
+            em.getTransaction().commit();
+
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+        } finally {
+            if (em.isOpen() && em.getTransaction().isActive())
+                em.close();
+        }
+
+    }
+
+    public void remove(Author author) {
+        EntityManager em = ConnectionFactory.getEntityManager();
+
+        try {
+            em.getTransaction().begin();
+            em.persist(author);
+            em.getTransaction().commit();
+
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+        } finally {
+            if (em.isOpen() && em.getTransaction().isActive())
+                em.close();
+        }
+
+    }
+
+    public Author findById(Integer code) {
+        EntityManager em = ConnectionFactory.getEntityManager();
+
+        try {
+            Author b = em.find(Author.class, code);
+            return b;
+        } finally {
+            if (em.isOpen() && em.getTransaction().isActive())
+                em.close();
+        }
+
     }
 }
