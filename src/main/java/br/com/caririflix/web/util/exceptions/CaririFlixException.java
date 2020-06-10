d962b9e81e98ea207d3f4f57b305d5972c84290b
Jarmeson Lacerda <jarmesonlacerda@users.noreply.github.com>
2020-06-09 21:16:11 -0300

    update class

---
+++ src/main/java/br/com/caririflix/web/util/exceptions/CaririFlixException.java
@@ -0,0 +1,12 @@
+package br.com.caririflix.web.util.exceptions;
+
+public class CaririFlixException extends Exception {
+
+    public CaririFlixException() {
+    }
+
+    public CaririFlixException(String message) {
+        super(message);
+    }
+
+}
