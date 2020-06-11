954fdb703282f3a3d678829499d0ce3cdd5de144
Silvan Mendes <silvanmendes49@gmail.com>
2020-06-11 18:08:04 -0300

    Correção e Atualização da Classe MoviesService

---
+++ src/main/java/br/com/caririflix/web/service/MoviesService.java
@@ -0,0 +1,29 @@
+package br.com.caririflix.web.service;
+
+import br.com.caririflix.web.DAO.MoviesDAO;
+import br.com.caririflix.web.model.Movies;
+import br.com.caririflix.web.util.exceptions.CaririFlixException;
+
+
+public class MoviesService {
+    MoviesDAO moviesDAO =  new MoviesDAO();
+    
+    public void save(Movies movies)throws CaririFlixException{
+        if(movies.getName() == null || movies.getName().isEmpty())
+            throw new CaririFlixException("Por favor insira o nome Filme");
+
+        if(movies.getGenre() == null || movies.getGenre().isEmpty())
+            throw new CaririFlixException("Por favor insira o Gênero");
+
+        if(movies.getRelease_date()== null || movies.getRelease_date().isEmpty())
+            throw new CaririFlixException("Por favor insira a Data de Lançamento");
+
+        if(movies.getDirection()== null || movies.getDirection().isEmpty())
+            throw new CaririFlixException("Por favor insira os Responsaveis pela Direção");
+
+        if(movies.getSynopsis()== null || movies.getSynopsis().isEmpty())
+            throw new CaririFlixException("Por favor insira uma Sinopse");
+        moviesDAO.save(movies);
+    }
+    
+}
